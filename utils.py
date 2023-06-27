c21099d5ba13701f7768ecd11d7ae16d9113f3f8
RodrigFallas <86426662+RodrigFallas@users.noreply.github.com>
2023-06-27 11:41:17 -0600

    GoodTest

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import unittest
 class TestBasica(unittest.TestCase):
 
     def test_es_menor_que(self):
-        self.assertTrue(10<5)
+        self.assertTrue(3<5)
 
 if __name__=="__main__":
     unittest.main()
